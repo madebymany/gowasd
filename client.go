a02234a0c04dbc8891cd27e5b4cef13548092656
Dan Brown <dan@madebymany.co.uk>
2015-04-29 18:10:14 +0100

    Initial commit

---
+++ client.go
@@ -0,0 +1,212 @@
+package gowasd
+
+import (
+	"github.com/miekg/dns"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+const DefaultPropertyVersion = 1
+
+type VersionedProperties map[int]map[string]string
+
+type Client struct {
+	c    *dns.Client
+	Addr string
+}
+
+func New(c *dns.Client, addr string) Client {
+	return Client{c: c, Addr: addr}
+}
+
+type Service struct {
+	Name     string
+	Protocol string
+	Domain   string
+}
+
+func (srv Service) DnsName() string {
+	return dumpDnsName(srv.DnsLabels())
+}
+
+func (srv Service) DnsLabels() []string {
+	return []string{"_" + srv.Name, "_" + srv.Protocol, srv.Domain}
+}
+
+type Instance struct {
+	Service
+	Description string
+	FullName    string
+}
+
+func (inst Instance) DnsName() string {
+	return dumpDnsName(inst.DnsLabels())
+}
+
+func (inst Instance) DnsLabels() []string {
+	return append([]string{inst.Description}, inst.Service.DnsLabels()...)
+}
+
+type Endpoint struct {
+	Host     string
+	Port     int
+	priority int
+}
+
+type EndpointList []Endpoint
+
+func (self EndpointList) Len() int {
+	return len(self)
+}
+
+func (self EndpointList) Less(i, j int) bool {
+	// Note we wanted this sorted descending, so we flip the inequality
+	return self[i].priority > self[j].priority
+}
+
+func (self EndpointList) Swap(i, j int) {
+	self[i], self[j] = self[j], self[i]
+}
+
+type InstanceResolution struct {
+	Instance
+	Targets    EndpointList
+	Properties VersionedProperties
+}
+
+func (self *Client) ServiceInstances(srv Service) (out []Instance, err error) {
+	msg := new(dns.Msg)
+	msg.SetQuestion(srv.DnsName(), dns.TypePTR)
+	resp, _, err := self.c.Exchange(msg, self.Addr)
+	if err != nil {
+		return nil, err
+	}
+
+	var parts []string
+	out = make([]Instance, len(resp.Answer))
+	for i, ans := range resp.Answer {
+		if ansPtr, ok := ans.(*dns.PTR); ok {
+			parts = parseDnsName(ansPtr.Ptr, 4)
+			out[i] = Instance{
+				Description: parts[0],
+				FullName:    ansPtr.Ptr,
+				Service: Service{
+					Name:     parts[1][1:],
+					Protocol: parts[2][1:],
+					Domain:   parts[3],
+				},
+			}
+		}
+	}
+
+	return
+}
+
+func (self *Client) ResolveInstance(inst Instance) (out InstanceResolution, err error) {
+	msg := new(dns.Msg)
+	n := inst.DnsName()
+	msg.SetQuestion(n, dns.TypeSRV)
+	msg.Question = append(msg.Question, dns.Question{
+		Name:   n,
+		Qtype:  dns.TypeTXT,
+		Qclass: dns.ClassINET,
+	})
+
+	resp, _, err := self.c.Exchange(msg, self.Addr)
+	if err != nil {
+		return
+	}
+
+	out.Instance = inst
+	out.Targets = make(EndpointList, 0, 3) // 3 is a fair guess!
+	out.Properties = make(VersionedProperties)
+
+	for _, anyRR := range resp.Answer {
+		switch rr := anyRR.(type) {
+		case *dns.SRV:
+			// TODO: weight
+			out.Targets = append(out.Targets, Endpoint{
+				Host:     rr.Target,
+				Port:     int(rr.Port),
+				priority: int(rr.Priority),
+			})
+		case *dns.TXT:
+			parseTxtRecordForProperties(rr, &out)
+		}
+	}
+
+	sort.Sort(out.Targets)
+
+	return
+}
+
+func parseTxtRecordForProperties(rr *dns.TXT, out *InstanceResolution) {
+	var propParts []string
+	var k, v string
+
+	propVersion := DefaultPropertyVersion
+
+	for i, prop := range rr.Txt {
+		propParts = strings.SplitN(prop, "=", 2)
+		if len(propParts) != 2 || propParts[0] == "" {
+			continue
+		}
+		k, v = propParts[0], propParts[1]
+
+		if i == 0 && k == "txtvers" {
+			pv64, err := strconv.ParseInt(v, 0, 64)
+			if err == nil {
+				propVersion = int(pv64)
+				continue
+			}
+		}
+
+		if _, ok := out.Properties[propVersion]; !ok {
+			out.Properties[propVersion] = make(map[string]string)
+		}
+
+		out.Properties[propVersion][k] = v
+	}
+
+}
+
+func parseDnsName(s string, n int) (out []string) {
+	// must be .-terminated
+
+	var escaped bool
+	var label string
+	out = make([]string, 0, 10)
+
+	for i, c := range s {
+		if c == '\\' {
+			escaped = true
+			continue
+		} else if c == '.' && !escaped {
+			out = append(out, label)
+			label = ""
+			escaped = false
+			if n > 0 && len(out) == n-1 {
+				out = append(out, s[i+1:len(s)-1])
+				break
+			}
+		} else {
+			escaped = false
+			label += string(c)
+		}
+	}
+
+	return
+}
+
+func dumpDnsName(n []string) (out string) {
+	for i, l := range n {
+		if i < len(n)-1 {
+			l = strings.Replace(l, "\\", "\\\\", -1)
+			l = strings.Replace(l, ".", "\\.", -1)
+			l = strings.Replace(l, " ", "\\ ", -1)
+		}
+		out += l + "."
+	}
+	return
+}
---
+++ client_test.go
@@ -0,0 +1,151 @@
+package gowasd
+
+import (
+	"github.com/miekg/dns"
+	"os"
+	"os/exec"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func startTestDnsServer() (cmd *exec.Cmd) {
+	cmd = exec.Command("dnsmasq",
+		"-p", "53533", "-d", "-C", "test_dnsmasq.conf")
+	err := cmd.Start()
+	if err != nil {
+		panic(err)
+	}
+	time.Sleep(time.Second / 10) // sorry
+	return
+}
+
+func stopTestDnsServer(cmd *exec.Cmd) {
+	err := cmd.Process.Signal(os.Interrupt)
+	if err != nil {
+		panic(err)
+	}
+	cmd.Wait()
+}
+
+const testDnsAddr = "127.0.0.1:53533"
+
+func TestParseDnsName(t *testing.T) {
+	var res []string
+
+	tryExample := func(s string, n int, exp []string) {
+		res = parseDnsName(s, n)
+		if !reflect.DeepEqual(res, exp) {
+			t.Errorf("tried %#v, expected %#v, got %#v", s, exp, res)
+		}
+	}
+
+	tryExample("_test._tcp.example.com.", 3,
+		[]string{"_test", "_tcp", "example.com"})
+	tryExample("Woop._test._tcp.example.com.", 4,
+		[]string{"Woop", "_test", "_tcp", "example.com"})
+	tryExample("Woop hello there._test._tcp.example.com.", 4,
+		[]string{"Woop hello there", "_test", "_tcp", "example.com"})
+	tryExample("Woop\\ hello\\ there._test._tcp.example.com.", 4,
+		[]string{"Woop hello there", "_test", "_tcp", "example.com"})
+}
+
+func TestDumpDnsName(t *testing.T) {
+	var res string
+
+	tryExample := func(s []string, exp string) {
+		res = dumpDnsName(s)
+		if res != exp {
+			t.Errorf("tried %#v, expected %#v, got %#v", s, exp, res)
+		}
+	}
+
+	tryExample([]string{"_test", "_tcp", "example.com"},
+		"_test._tcp.example.com.")
+	tryExample([]string{"Woo", "_test", "_tcp", "example.com"},
+		"Woo._test._tcp.example.com.")
+	tryExample([]string{"Woo yay", "_test", "_tcp", "example.com"},
+		"Woo\\ yay._test._tcp.example.com.")
+	tryExample([]string{"1. this is awesome", "_test", "_tcp", "example.com"},
+		"1\\.\\ this\\ is\\ awesome._test._tcp.example.com.")
+}
+
+func TestClient_ServiceInstances(t *testing.T) {
+	dnsmasq := startTestDnsServer()
+	c := New(new(dns.Client), testDnsAddr)
+
+	insts, err := c.ServiceInstances(Service{
+		Name:     "test",
+		Protocol: "tcp",
+		Domain:   "example.com",
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected :=
+		[]Instance{
+			Instance{
+				Service:     Service{Name: "test", Protocol: "tcp", Domain: "example.com"},
+				Description: "Woop",
+				FullName:    "Woop._test._tcp.example.com."},
+			Instance{
+				Service:     Service{Name: "test", Protocol: "tcp", Domain: "example.com"},
+				Description: "Hello There",
+				FullName:    "Hello\\ There._test._tcp.example.com.",
+			}}
+
+	if !reflect.DeepEqual(insts, expected) {
+		t.Errorf("expected %#v, got %#v", expected, insts)
+	}
+
+	stopTestDnsServer(dnsmasq)
+}
+
+func TestClient_ResolveInstance(t *testing.T) {
+	dnsmasq := startTestDnsServer()
+	c := New(new(dns.Client), testDnsAddr)
+
+	inst := Instance{
+		Service:     Service{Name: "test", Protocol: "tcp", Domain: "example.com"},
+		Description: "Woop",
+		FullName:    "Woop._test._tcp.example.com.",
+	}
+
+	resInst, err := c.ResolveInstance(inst)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expected := InstanceResolution{
+		Instance: Instance{
+			Service: Service{
+				Name:     "test",
+				Protocol: "tcp",
+				Domain:   "example.com"},
+			Description: "Woop",
+			FullName:    "Woop._test._tcp.example.com.",
+		},
+		Targets: EndpointList{
+			Endpoint{
+				Host:     "woop.example.com.",
+				Port:     49153,
+				priority: 0,
+			}},
+		Properties: VersionedProperties{
+			1: map[string]string{
+				"hello": "there",
+				"this":  "is=fun",
+			},
+			2: map[string]string{
+				"second": "version",
+				"gosh":   "wow",
+			},
+		}}
+
+	if !reflect.DeepEqual(resInst, expected) {
+		t.Errorf("expected %#v, got %#v", expected, resInst)
+	}
+
+	stopTestDnsServer(dnsmasq)
+}
